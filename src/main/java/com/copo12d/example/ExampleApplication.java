a09127d203e034750320471613b9b6cee9d3bfa2
Angel <acalmenc@gmail.com>
2025-02-18 13:04:17 -0400

    Environment Bean

--- src/main/java/com/copo12d/example/ExampleApplication.java
+++ src/main/java/com/copo12d/example/ExampleApplication.java
@@ -12,6 +12,10 @@ public class ExampleApplication {
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
+		System.out.println(myFirstService.getJavaVersion());
+		System.out.println(myFirstService.getOsName());
+		System.out.println(myFirstService.getApplicationName());
+		System.out.println(myFirstService.getCustomProperty());
 	}
 
 	//@Bean
--- src/main/java/com/copo12d/example/MyFirstService.java
+++ src/main/java/com/copo12d/example/MyFirstService.java
@@ -2,20 +2,39 @@ package com.copo12d.example;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyFirstService {
 
     private MyFirstClass myFirstClass;
+    private Environment environment;
     //Setter injection used to inject dependencies using setters.
     @Autowired
     public void setMyFirstClass(@Qualifier("mySecondClass") MyFirstClass myFirstClass){
         this.myFirstClass = myFirstClass;
     }
 
-
     public String tellAStory(){
         return "The dependency is saying : " + myFirstClass.sayHello();
+    }
+
+    public String getJavaVersion(){
+        return environment.getProperty("java.version");
+    }
+    public String getOsName(){
+        return environment.getProperty("os.name");
+    }
+    public String getApplicationName(){
+        return environment.getProperty("spring.application.name");
+    }
+    public String getCustomProperty(){
+        return environment.getProperty("my.custom.property");
+    }
+
+    @Autowired
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
     }
 }
