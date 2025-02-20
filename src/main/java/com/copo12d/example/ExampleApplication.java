7f098cbb9c0db0dd92ecb852387f7b492a3e01bf
Angel <acalmenc@gmail.com>
2025-02-19 21:37:24 -0400

    Properties profiles

--- src/main/java/com/copo12d/example/ExampleApplication.java
+++ src/main/java/com/copo12d/example/ExampleApplication.java
@@ -4,11 +4,16 @@ package com.copo12d.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collection;
+import java.util.Collections;
+
 @SpringBootApplication
 public class ExampleApplication {
 
 	public static void main(String[] args) {
-		var ctx = SpringApplication.run(ExampleApplication.class, args);
+		var app = new SpringApplication(ExampleApplication.class);
+		app.setAdditionalProfiles("dev");
+		var ctx = app.run(args);
 
 		MyFirstService myFirstService = ctx.getBean(MyFirstService.class);
 		System.out.println(myFirstService.tellAStory());
