5ffb0dcec696665bfa7f9b005d377dac8bd2591f
Angel <acalmenc@gmail.com>
2025-02-21 18:26:27 -0400

    PathVariables

--- src/main/java/com/copo12d/example/FirstController.java
+++ src/main/java/com/copo12d/example/FirstController.java
@@ -1,39 +1,42 @@
 package com.copo12d.example;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 public class FirstController {
-    private final List<OrderDTO> orders = new ArrayList<>();
 
+    //GET METHODS
     @GetMapping
     public String sayHello() {
         return "Hello World";
+    }
+    //http://localhost:8080/hello/(What ever ypu want).
+    @GetMapping("/hello/{user-name}")
+    public String pathVar(@PathVariable("user-name") String username) {
+        return "my value: " + username;
+    }
+    //http://localhost:8080/hello?param_name=param_value&param_name2=param_value
+    @GetMapping("/hello2")
+    public String paramVar(@RequestParam("user-name") String username, @RequestParam("password") String password) {
+        return "my value: " + username + " " + password;
+    }
 
-    }
-    @GetMapping("/products")
-    public List<OrderDTO> getProductName() {
-        return orders;
-    }
+    //POST METHODS
    @PostMapping("/post")
     public String Post(@RequestBody Map<String, Object> message) {
         return "Request accepted and message is: " + message.get("message");
    }
+
     @PostMapping("/post-order")
     public String order(@RequestBody OrderDTO order) {
-        orders.add(order);
-        return "Request accepted and order is: " + order.getCustomerName();
+        return "Request accepted and order is: " + order.toString();
     }
+
     @PostMapping("/post-order-record")
     public String record(@RequestBody OrderRecord order) {
         return "Request accepted and order is: " + order.toString();
     }
+
 }
